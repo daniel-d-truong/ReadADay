8d96a344a6035de91154a963f8d2b33ddbcd4b83
Matt Vav <mathvav@users.noreply.github.com>
2020-06-27 17:08:40 -0500

    Bootstrap node server

---
+++ news-server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const serverPort = 8000;
+
+app.get("/", (request, response) => {
+    response.send("Hello, world!");
+});
+
+app.listen(serverPort, () => console.log(`Server started on port ${serverPort}!`));
