eddc94edb2ec3f6654148862c0b0a58c89053411
Daniel Truong <ddtruong06@gmail.com>
2020-06-27 21:03:18 -0700

    Setting up API

--- news-ios/news-ios/Util/API.swift
+++ news-ios/news-ios/Util/API.swift
@@ -8,31 +8,59 @@
 
 import Foundation
 
+let backendURL = "http://localhost:8000";
+
 class APIFunctions {
     // Private Init. DO NOT MAKE AN OBJECT FROM THIS
     init() {}
     
     func getArticlesFeed(_ completion: @escaping ((_ articlesList: [Article]) -> Void)) {
-        var articlesList: [Article] = []
+//        var articlesList: [Article] = []
         
-        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
-        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
-        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
+//        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
+//        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
+//        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
+  
+        let url = URL(string: "\(backendURL)/articles")
+        guard let requestUrl = url else { fatalError() }
         
-        completion(articlesList)
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "GET"
+        
+        
+        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            // check if error took place
+            if let error = error {
+                print("Error took place")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Response HTTP status code: \(response.statusCode)")
+            }
+            
+            // Convert Data to String
+            if let data = data {
+                let someArray: [[String: String]] = []
+                let articlesList: [Article] = someArray.map{ Article($0) }
+                completion(articlesList)
+            }
+        }
     }
     
     func getArticlesHistory(_ completion: @escaping ((_ articlesList: [Article]) -> Void)) {
         var articlesList: [Article] = []
         
-        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
-        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
-        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals"))
+        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals", id: "some string"))
+        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals", id: "some string"))
+        articlesList.append(Article(url: "https://markmanson.net/best-articles", title: "Cat", imageSrc: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", category: "Animals", id: "some string") )
         
         completion(articlesList)
     }
     
     func submitArticle(articleURL: String, _ successCompletion: @escaping (() -> Void), _ failureCompletion: @escaping (() -> Void)) {
+        
+        // TODO: Put logic down
         successCompletion()
     }
 }
--- news-ios/news-ios/Util/Article.swift
+++ news-ios/news-ios/Util/Article.swift
@@ -13,11 +13,17 @@ class Article {
     var title: String
     var imageSrc: String
     var category: String
+    var id: String
     
-    init(url: String, title: String, imageSrc: String, category: String) {
+    init(url: String, title: String, imageSrc: String, category: String, id: String) {
         self.url = url
         self.title = title
         self.imageSrc = imageSrc
         self.category = category
+        self.id = id
+    }
+    
+    convenience init(_ dict: [String: String]) {
+        self.init(url: dict["URL"]!, title: dict["Title"]!, imageSrc: dict["ImageURL"]!, category: dict["Category"]!, id: dict["ID"]!)
     }
 }
